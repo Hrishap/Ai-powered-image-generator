18b20b2f6cbd5ff23f83a640ee13c5b470688987
Hrishap <121787992+Hrishap@users.noreply.github.com>
2024-04-04 22:22:30 +0530

    first commit

---
+++ src/app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TransformationsPage = () => {
+  return (
+    <div>
+      Transformation page
+    </div>
+  )
+}
+
+export default TransformationsPage
---
+++ src/app/(root)/transformations/[id]/update/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UpdateTransformationPage = () => {
+  return (
+    <div>
+      Update transformation page
+    </div>
+  )
+}
+
+export default UpdateTransformationPage
